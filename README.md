bee0c8be52cabbb37f853e701e5905431c6376f2
Trupti Patel <truppatel85@gmail.com>
2019-11-26 11:28:59 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# UKBA-FrontEnd-Serenity
---
+++ src/main/java/uk/visa/gov/serenity/cucumber/App.java
@@ -0,0 +1,13 @@
+package uk.visa.gov.serenity.cucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
